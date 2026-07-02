8593b36d67e4c7a8946c3df1601d78de6c0e6f97
caetano.cesar <caetano.cesar@defensoria.mg.def.br>
2026-07-02 10:00:28 -0300

    fix: deploy form next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // Gera arquivos HTML/CSS estáticos
+  basePath: "/landing_page_pessoal", // Descomente esta linha se NÃO for usar um domínio próprio
   images: {
+    unoptimized: true, // O GitHub Pages não tem servidor Node para otimizar imagens
     remotePatterns: [
       {
         protocol: "https",
